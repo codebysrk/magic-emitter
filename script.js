88159af5b83b881fbb1e44ab37a9b19e6c359769
Shahrukh Khan <153599461+codebysrk@users.noreply.github.com>
2025-12-19 14:28:16 +0530

    initial

---
+++ script.js
@@ -0,0 +1,68 @@
+const emoji = [
+  "🚀",
+  "✨",
+  "🌟",
+  "💫",
+  "🌈",
+  "🔥",
+  "🎉",
+  "💖",
+  "💡",
+  "🔮",
+  "💎",
+  "💯",
+  "🤩",
+  "🥳",
+  "🤯",
+  "😎",
+  "😇",
+  "🥰",
+  "🍓",
+  "🍕",
+  "🎵",
+  "🎈",
+  "🌊",
+  "⚡",
+  "🦋",
+  "🍭",
+  "🎨",
+  "🛸",
+];
+
+const main = document.querySelector(".main");
+const btn = document.querySelector(".btn");
+
+const getRandom = (max, min = 0) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const addName = (e) => {
+  const el = document.createElement("div");
+  el.className = "emoji-item";
+
+  const randomEmoji = emoji[getRandom(emoji.length - 1)];
+  el.textContent = randomEmoji;
+
+  const x = getRandom(90, 5);
+  const y = getRandom(90, 5);
+
+  const scale = getRandom(30, 15) / 10;
+  const rotation = getRandom(45, -45);
+
+  el.style.left = `${x}%`;
+  el.style.top = `${y}%`;
+  el.style.fontSize = `${getRandom(2, 4)}rem`;
+
+  el.style.setProperty("--final-scale", scale);
+  el.style.setProperty("--final-rotation", `${rotation}deg`);
+
+  const headings = document.querySelectorAll(".emoji-item");
+  if (headings.length > 50) {
+    headings[0].remove();
+  }
+
+  main.appendChild(el);
+};
+
+btn.addEventListener("click", addName);
+
+setTimeout(addName, 500);
